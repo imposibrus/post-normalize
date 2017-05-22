7cc395cd0ee7786cca2315bd8eb82636b6e2d759
imposibrus <imposibrus@gmail.com>
2017-05-23 00:00:51 +0300

    fix - codestyle fix - update dependencies fix - add usage example to readme

--- index.js
+++ index.js
@@ -2,22 +2,26 @@
 var formidable = require('formidable');
 
 module.exports = function postNormalize() {
-  return function postNormalizeHandler(req, res, next) {
-    if(/multipart\/form-data/.test(req.get('content-type'))) {
-      var form = new formidable.IncomingForm();
-      form.hash = 'md5';
-      form.parse(req, function(err, fields, files) {
-        if(err) {
-          return next(err);
+    return function postNormalizeHandler(req, res, next) {
+        if (!/multipart\/form-data/.test(req.get('content-type'))) {
+            next();
+
+            return;
         }
 
-        req.files = files;
-        req.body = fields;
-        next();
-      });
-    } else {
-      next();
+        var form = new formidable.IncomingForm();
+
+        form.hash = 'md5';
+
+        form.parse(req, function(err, fields, files) {
+            if (err) {
+                return next(err);
+            }
+
+            req.files = files;
+            req.body = fields;
+            next();
+        });
     }
-  }
 };
 
--- test/index.js
+++ test/index.js
@@ -12,64 +12,54 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(postNormalize());
 app.post('*', function(req, res) {
-  res.send({body: req.body, files: req.files});
+    res.send({body: req.body, files: req.files});
 });
 
 before(function(done) {
-  server = app.listen(process.env.PORT || 0, done);
+    server = app.listen(process.env.PORT || 0, done);
 });
 after(function(done) {
-  tempFiles.forEach(function(path) {
-    fs.unlinkSync(path);
-  });
-  server.close(done);
+    tempFiles.forEach(function(path) {
+        fs.unlinkSync(path);
+    });
+    server.close(done);
 });
 
 describe('post-normalize', function() {
-  it('should parse body, populate fields and save image in temp folder', function(done) {
-    request(server)
-        .post('/')
-        .field('key', 'value')
-        .attach('image', path.resolve(__dirname, 'fixtures/nodejs.jpg'))
-        .expect(200)
-        .end(function(err, res) {
-          if(err) {
-            return done(err);
-          }
+    it('should parse body, populate fields and save image in temp folder', function() {
+        return request(server)
+            .post('/')
+            .field('key', 'value')
+            .attach('image', path.resolve(__dirname, 'fixtures/nodejs.jpg'))
+            .expect(200)
+            .then(function(res) {
+                res.body.should.have.properties(['body', 'files']);
+                res.body.body.should.have.property('key');
+                res.body.body.key.should.be.equal('value');
+                res.body.files.should.have.property('image');
+                res.body.files.image.should.have.properties(['path', 'mtime']);
+                res.body.files.image.should.have.properties({
+                    size: 46575,
+                    name: 'nodejs.jpg',
+                    type: 'image/jpeg'
+                });
+                fs.existsSync(res.body.files.image.path).should.be.equal(true);
+                tempFiles.push(res.body.files.image.path);
+            });
+    });
 
-          res.body.should.have.properties(['body', 'files']);
-          res.body.body.should.have.property('key');
-          res.body.body.key.should.be.equal('value');
-          res.body.files.should.have.property('image');
-          res.body.files.image.should.have.properties(['path', 'mtime']);
-          res.body.files.image.should.have.properties({
-            size: 46575,
-            name: 'nodejs.jpg',
-            type: 'image/jpeg'
-          });
-          fs.existsSync(res.body.files.image.path).should.be.equal(true);
-          tempFiles.push(res.body.files.image.path);
-          done();
-        });
-  });
-
-  it('should do nothing on `application/x-www-form-urlencoded` request', function(done) {
-    request(server)
-        .post('/')
-        .send({key: 'value'})
-        .expect(200)
-        .end(function(err, res) {
-          if(err) {
-            return done(err);
-          }
-
-          res.body.should.have.property('body');
-          res.body.should.not.have.property('files');
-          res.body.body.should.have.property('key');
-          res.body.body.key.should.be.equal('value');
-          done();
-        });
-  });
+    it('should do nothing on `application/x-www-form-urlencoded` request', function() {
+        return request(server)
+            .post('/')
+            .send({key: 'value'})
+            .expect(200)
+            .then(function(res) {
+                res.body.should.have.property('body');
+                res.body.should.not.have.property('files');
+                res.body.body.should.have.property('key');
+                res.body.body.key.should.be.equal('value');
+            });
+    });
 
 });
 
